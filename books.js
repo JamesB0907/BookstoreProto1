1ef50cd5195df6e0eeeba5dbc1f97a4baa8b228a
James Brooks <virverum22@gmail.com>
2025-02-18 15:41:59 -0500

    part 2

---
+++ books.js
@@ -0,0 +1,72 @@
+const books = [
+    {
+        id: 1,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        price: 7.99,
+        image: ''
+    },
+    {
+        id: 2,
+        title: 'The Lord of the Rings: The Two Towers',
+        author: 'J.R.R. Tolkien',
+        price: 9.99,
+        image: ''
+    },
+    {
+        id: 3,
+        title: '1984',
+        author: 'George Orwell',
+        price: 6.99,
+        image: ''
+    },
+    {
+        id: 4,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        price: 8.99,
+        image: ''
+    },
+    {
+        id: 5,
+        title: 'Pride and Prejudice',
+        author: 'Jane Austen',
+        price: 5.99,
+        image: ''
+    },
+    {
+        id: 6,
+        title: 'The Catcher in the Rye',
+        author: 'J.D. Salinger',
+        price: 6.49,
+        image: ''
+    },
+    {
+        id: 7,
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        price: 7.49,
+        image: ''
+    },
+    {
+        id: 8,
+        title: 'Moby Dick',
+        author: 'Herman Melville',
+        price: 9.49,
+        image: ''
+    },
+    {
+        id: 9,
+        title: 'War and Peace',
+        author: 'Leo Tolstoy',
+        price: 12.99,
+        image: ''
+    },
+    {
+        id: 10,
+        title: 'The Odyssey',
+        author: 'Homer',
+        price: 10.99,
+        image: ''
+    }
+];
